b979e52bc9022cfe3ea336e747a7b815da1093cd
JaumeMiroCorcoles1 <jmircor@upv.edu.es>
2026-06-18 17:46:33 +0200

    API Proyecto 2

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/estado", (req, res) => {
+    res.json({
+        puerta: "ABIERTA",
+        prensa: "TRABAJANDO",
+        energia: 75
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado en puerto " + PORT);
+});
